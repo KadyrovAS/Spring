26224706b57a8afaf25617f723fd0ce32389cc54
Andrey <kadyrovas@yandex.ru>
2021-07-31 19:02:11 +0300

    my first spring project

--- src/main/java/h2/MainH2.java
+++ src/main/java/h2/MainH2.java
@@ -17,59 +17,105 @@ public class MainH2 {
         return arg.substring(1, arg.length()-1);
     }
 
-    private static int commandBalance(String[] commands, UserAccountService service) throws IOException, UnknownAccountException {
+    private static int commandBalance(String[] commands, UserAccountService service) throws IOException {
         if (commands.length != 2)
             return -1;
-        int id = Integer.valueOf(parseArg(commands[1]));
-        int amount = service.balance(id);
+        String arg = parseArg(commands[1]);
+        if (arg == null)
+            return -1;
+        int id = Integer.valueOf(arg);
+        int amount;
+        try {
+            amount = service.balance(id);
+        }catch (UnknownAccountException e){
+            return -1;
+        }
         return amount;
     }
 
-    private static int commandWithdraw(String[] commands, UserAccountService service) throws IOException, UnknownAccountException, NotEnoughMoneyException {
+    private static int commandWithdraw(String[] commands, UserAccountService service) throws IOException {
         if (commands.length !=3)
             return -1;
-        int id = Integer.valueOf(parseArg(commands[1]));
-        int amount = Integer.valueOf(parseArg(commands[2]));
-        service.withdraw(id, amount);
+        String arg = parseArg(commands[1]);
+        if (arg == null)
+            return -1;
+        int id=Integer.valueOf(arg);
+        arg = parseArg(commands[2]);
+        if (arg == null)
+            return -1;
+        int amount = Integer.valueOf(arg);
+        try {
+            service.withdraw(id, amount);
+        }catch (UnknownAccountException e){
+            return -1;
+        }catch (NotEnoughMoneyException e){
+            return -1;
+        }
+        System.out.println("Ваша команда выполнена корректно");
         return 0;
     }
 
-    private static int commandDeposit(String[] commands, UserAccountService service) throws IOException, UnknownAccountException, NotEnoughMoneyException {
+    private static int commandDeposit(String[] commands, UserAccountService service) throws IOException  {
         if (commands.length !=3)
             return -1;
-        int id = Integer.valueOf(parseArg(commands[1]));
-        int amount = Integer.valueOf(parseArg(commands[2]));
-        service.deposit(id, amount);
+        String arg = parseArg(commands[1]);
+        if (arg == null)
+            return -1;
+        int id = Integer.valueOf(arg);
+        arg = parseArg(commands[2]);
+        int amount = Integer.valueOf(arg);
+        try {
+            service.deposit(id, amount);
+        }catch (UnknownAccountException e){
+            return -1;
+        }
+        System.out.println("Ваша команда выполнена корректно");
         return 0;
     }
 
-    private static int commandTransfer(String[] commands, UserAccountService service) throws IOException, UnknownAccountException, NotEnoughMoneyException {
+    private static int commandTransfer(String[] commands, UserAccountService service) throws IOException {
         if (commands.length !=4)
             return -1;
-        int idFrom = Integer.valueOf(parseArg(commands[1]));
-        int idTo = Integer.valueOf(parseArg(commands[2]));
-        int amount = Integer.valueOf(parseArg(commands[3]));
-        service.transfer(idFrom, idTo, amount);
+        String arg = parseArg(commands[1]);
+        if (arg == null)
+            return -1;
+        int idFrom = Integer.valueOf(arg);
+        arg = parseArg(commands[2]);
+        if (arg == null)
+            return -1;
+        int idTo = Integer.valueOf(arg);
+        arg = parseArg(commands[3]);
+        if (arg == null)
+            return -1;
+        int amount = Integer.valueOf(arg);
+        try {
+            service.transfer(idFrom, idTo, amount);
+        }catch (UnknownAccountException e){
+            return -1;
+        }catch (NotEnoughMoneyException e){
+            return -1;
+        }
+        System.out.println("Ваша команда выполнена корректно");
         return 0;
     }
 
     private static int parsLine(String line, UserAccountService service) throws IOException, UnknownAccountException, NotEnoughMoneyException {
         String[] commands = line.split(" ");
         if (commands[0].toUpperCase().compareTo("BALANCE") == 0){
-            System.out.println("Баланс: " + commandBalance(commands, service));
+            int amount = commandBalance(commands, service);
+            if (amount < 0)
+                return -1;
+            System.out.println("Баланс: " + amount);
             return 0;
         }
         else if (commands[0].toUpperCase().compareTo("WITHDRAW") == 0){
-            commandWithdraw(commands, service);
-            return 0;
+            return commandWithdraw(commands, service);
         }
         else if (commands[0].toUpperCase().compareTo("DEPOSIT") == 0){
-            commandDeposit(commands, service);
-            return 0;
+            return commandDeposit(commands, service);
         }
         else if (commands[0].toUpperCase().compareTo("TRANSFER") == 0){
-            commandTransfer(commands, service);
-            return 0;
+            return commandTransfer(commands, service);
         }
         else
             return -1;
@@ -80,13 +126,16 @@ public class MainH2 {
         UserAccountService service = (UserAccountService) context.getBean("accountService");
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
 //        service.store.read().forEach(System.out::println);
-       while (true){
+
+        while (true){
            System.out.print("Введите команду: ");
            String line = bufferedReader.readLine();
            if (line.trim().toUpperCase().compareTo("EXIT") == 0)
                break;
            if (parsLine(line, service) != 0)
                System.out.println("Вы ошиблись при вводе команды.");
+           else
+               service.store.write(service.store.read());
        }
     }
 }
