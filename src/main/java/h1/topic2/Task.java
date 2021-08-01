7a639bc19dd281468241aebef1cbc1ff7f10606d
Andrey <kadyrovas@yandex.ru>
2021-08-01 06:06:04 +0300

    My first project in Spring

---
+++ src/main/java/h1/topic2/Task.java
@@ -0,0 +1,9 @@
+package h1.topic2;
+
+public class Task {
+    private String id;
+    private String description;
+    private String author;
+    private String name;
+    private int storyPoint;
+}
